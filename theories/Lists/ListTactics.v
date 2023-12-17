0332b7b7ed686733a636790fa0df4f9713b87091
Pierre Roux <pierre@roux01.fr>
2023-12-17 18:29:19 +0100

    Add Makefile for list

--- theories/Lists/ListTactics.v
+++ theories/Lists/ListTactics.v
@@ -8,7 +8,7 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-Require Import BinNums.PosDef.
+From Stdlib.BinNums Require Import PosDef.
 Require Import List.
 
 Ltac list_fold_right fcons fnil l :=
--- theories/Streams/StreamMemo.v
+++ theories/Streams/StreamMemo.v
@@ -8,7 +8,7 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-Require Import Eqdep_dec.
+From Stdlib.Logic Require Import Eqdep_dec.
 Require Import Streams.
 
 (** * Memoization *)
