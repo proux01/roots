jpoiret/coq

Restrict dependencies of axioms specifying primitive operators

theories/Array/PArray.v
@@ -1,68 +1,8 @@
-Require Import Uint63.
-
-Set Universe Polymorphism.
-
-Primitive array := #array_type.
-
-Primitive make : forall A, int -> A -> array A := #array_make.
-Arguments make {_} _ _.
-
-Primitive get : forall A, array A -> int -> A := #array_get.
-Arguments get {_} _ _.
-
-Primitive default : forall A, array A -> A:= #array_default.
-Arguments default {_} _.
-
-Primitive set : forall A, array A -> int -> A -> array A := #array_set.
-Arguments set {_} _ _ _.
-
-Primitive length : forall A, array A -> int := #array_length.
-Arguments length {_} _.
-
-Primitive copy : forall A, array A -> array A := #array_copy.
-Arguments copy {_} _.
-
-Module Export PArrayNotations.
-
-Declare Scope array_scope.
-Delimit Scope array_scope with array.
-Notation "t .[ i ]" := (get t i)
-  (at level 2, left associativity, format "t .[ i ]").
-Notation "t .[ i <- a ]" := (set t i a)
-  (at level 2, left associativity, format "t .[ i <- a ]").
-
-End PArrayNotations.
+From Coq Require Import Uint63.
+From Coq Require Export PrimArray ArrayAxioms.
 
 Local Open Scope uint63_scope.
 Local Open Scope array_scope.
-
-Primitive max_length := #array_max_length.
-
-(** Axioms *)
-Axiom get_out_of_bounds : forall A (t:array A) i, (i <? length t) = false -> t.[i] = default t.
-
-Axiom get_set_same : forall A t i (a:A), (i <? length t) = true -> t.[i<-a].[i] = a.
-Axiom get_set_other : forall A t i j (a:A), i <> j -> t.[i<-a].[j] = t.[j].
-Axiom default_set : forall A t i (a:A), default t.[i<-a] = default t.
-
-
-Axiom get_make : forall A (a:A) size i, (make size a).[i] = a.
-
-Axiom leb_length : forall A (t:array A), length t <=? max_length = true.
-
-Axiom length_make : forall A size (a:A),
-  length (make size a) = if size <=? max_length then size else max_length.
-Axiom length_set : forall A t i (a:A),
-  length t.[i<-a] = length t.
-
-Axiom get_copy : forall A (t:array A) i, (copy t).[i] = t.[i].
-Axiom length_copy : forall A (t:array A), length (copy t) = length t.
-
-Axiom array_ext : forall A (t1 t2:array A),
-  length t1 = length t2 ->
-  (forall i, i <? length t1 = true -> t1.[i] = t2.[i]) ->
-  default t1 = default t2 ->
-  t1 = t2.
 
 (* Lemmas *)
 

theories/Array/PrimArray.v
@@ -1,0 +1,36 @@
+From Coq Require Import PrimInt63.
+
+Set Universe Polymorphism.
+
+Primitive array := #array_type.
+
+Primitive make : forall A, int -> A -> array A := #array_make.
+Arguments make {_} _ _.
+
+Primitive get : forall A, array A -> int -> A := #array_get.
+Arguments get {_} _ _.
+
+Primitive default : forall A, array A -> A:= #array_default.
+Arguments default {_} _.
+
+Primitive set : forall A, array A -> int -> A -> array A := #array_set.
+Arguments set {_} _ _ _.
+
+Primitive length : forall A, array A -> int := #array_length.
+Arguments length {_} _.
+
+Primitive copy : forall A, array A -> array A := #array_copy.
+Arguments copy {_} _.
+
+Module Export PArrayNotations.
+
+Declare Scope array_scope.
+Delimit Scope array_scope with array.
+Notation "t .[ i ]" := (get t i)
+  (at level 2, left associativity, format "t .[ i ]").
+Notation "t .[ i <- a ]" := (set t i a)
+  (at level 2, left associativity, format "t .[ i <- a ]").
+
+End PArrayNotations.
+
+Primitive max_length := #array_max_length.

theories/BinNums/NatDef.v
@@ -1,0 +1,127 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+From Coq Require Export BinNums PosDef.
+
+(**********************************************************************)
+(** * Binary natural numbers, definitions of operations *)
+(**********************************************************************)
+
+Module N.
+
+(** ** Operation [x -> 2*x+1] *)
+
+Definition succ_double x :=
+  match x with
+  | N0 => Npos 1
+  | Npos p => Npos p~1
+  end.
+
+(** ** Operation [x -> 2*x] *)
+
+Definition double n :=
+  match n with
+  | N0 => N0
+  | Npos p => Npos p~0
+  end.
+
+(** ** The successor of a [N] can be seen as a [positive] *)
+
+Definition succ_pos (n : N) : positive :=
+ match n with
+   | N0 => xH
+   | Npos p => Pos.succ p
+ end.
+
+(** Subtraction *)
+
+Definition sub n m :=
+match n, m with
+| N0, _ => N0
+| n, N0 => n
+| Npos n', Npos m' =>
+  match Pos.sub_mask n' m' with
+  | Pos.IsPos p => Npos p
+  | _ => N0
+  end
+end.
+
+(** Order *)
+
+Definition compare n m :=
+  match n, m with
+  | N0, N0 => Eq
+  | N0, Npos m' => Lt
+  | Npos n', N0 => Gt
+  | Npos n', Npos m' => Pos.compare n' m'
+  end.
+
+(** Boolean equality and comparison *)
+
+Definition leb x y :=
+ match compare x y with Gt => false | _ => true end.
+
+(** Euclidean division *)
+
+Fixpoint pos_div_eucl (a:positive)(b:N) : N * N :=
+  match a with
+    | xH =>
+       match b with Npos 1 => (Npos 1, N0) | _ => (N0, Npos 1) end
+    | xO a' =>
+       let (q, r) := pos_div_eucl a' b in
+       let r' := double r in
+       if leb b r' then (succ_double q, sub r' b)
+        else (double q, r')
+    | xI a' =>
+       let (q, r) := pos_div_eucl a' b in
+       let r' := succ_double r in
+       if leb b r' then (succ_double q, sub r' b)
+        else  (double q, r')
+  end.
+
+(** Operation over bits of a [N] number. *)
+
+(** Logical [or] *)
+
+Definition lor n m :=
+ match n, m with
+   | N0, _ => m
+   | _, N0 => n
+   | Npos p, Npos q => Npos (Pos.lor p q)
+ end.
+
+(** Logical [and] *)
+
+Definition land n m :=
+ match n, m with
+  | N0, _ => N0
+  | _, N0 => N0
+  | Npos p, Npos q => Pos.land p q
+ end.
+
+(** Logical [diff] *)
+
+Definition ldiff n m :=
+ match n, m with
+  | N0, _ => N0
+  | _, N0 => n
+  | Npos p, Npos q => Pos.ldiff p q
+ end.
+
+(** [xor] *)
+
+Definition lxor n m :=
+  match n, m with
+    | N0, _ => m
+    | _, N0 => n
+    | Npos p, Npos q => Pos.lxor p q
+  end.
+
+End N.

theories/Numbers/Cyclic/Int63/Sint63Axioms.v
@@ -1,0 +1,44 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+(*            Benjamin Gregoire, Laurent Thery, INRIA, 2007             *)
+(************************************************************************)
+
+From Coq Require Import BinNums PosDef IntDef.
+From Coq Require Export PrimInt63 Uint63Axioms.
+
+Local Open Scope Z_scope.
+
+Local Notation "2" := (Zpos 2) : Z_scope.
+Local Infix "^" := Z.pow : Z_scope.
+Local Notation "x <= y" := (Z.compare x y <> Gt) : Z_scope.
+Local Notation "x < y" := (Z.compare x y = Lt) : Z_scope.
+
+Definition min_int := Eval vm_compute in (lsl 1 62).
+
+(** Translation to and from Z *)
+Definition to_Z (i : int) :=
+  if ltb i min_int then to_Z i
+  else Z.opp (to_Z (sub 0 i)).
+
+(** Specification of operations that differ on signed and unsigned ints *)
+
+Axiom asr_spec : forall x p, to_Z (asr x p) = Z.div (to_Z x) (2 ^ (to_Z p)).
+
+Axiom div_spec : forall x y,
+    to_Z x <> to_Z min_int \/ to_Z y <> Zneg 1 ->
+  to_Z (divs x y) = Z.quot (to_Z x) (to_Z y).
+
+Axiom mod_spec : forall x y, to_Z (mods x y) = Z.rem (to_Z x) (to_Z y).
+
+Axiom ltb_spec : forall x y, ltsb x y = true <-> to_Z x < to_Z y.
+
+Axiom leb_spec : forall x y, lesb x y = true <-> to_Z x <= to_Z y.
+
+Axiom compare_spec : forall x y, eqb x y = Z.eqb (to_Z x) (to_Z y).

theories/Numbers/Cyclic/Int63/Uint63Axioms.v
@@ -1,0 +1,175 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+(*            Benjamin Gregoire, Laurent Thery, INRIA, 2007             *)
+(************************************************************************)
+
+From Coq Require Import BinNums PosDef IntDef.
+From Coq Require Export PrimInt63.
+
+Local Open Scope Z_scope.
+
+Local Notation "0" := Z0 : Z_scope.
+Local Notation "1" := (Zpos 1) : Z_scope.
+Local Notation "2" := (Zpos 2) : Z_scope.
+Local Infix "+" := Z.add : Z_scope.
+Local Infix "-" := Z.sub : Z_scope.
+Local Infix "*" := Z.mul : Z_scope.
+Local Infix "^" := Z.pow : Z_scope.
+Local Notation "x <= y" := (Z.compare x y <> Gt) : Z_scope.
+Local Notation "x < y" := (Z.compare x y = Lt) : Z_scope.
+Local Notation "x <= y < z" := (x <= y /\ y < z) : Z_scope.
+
+Definition size := 63%nat.
+
+(** The number of digits as an int *)
+Definition digits := 63%uint63.
+
+(** The biggest int *)
+Definition max_int := Eval vm_compute in sub 0 1.
+
+(** Access to the nth digits *)
+Definition get_digit x p := ltb 0 (land x (lsl 1 p)).
+
+Definition set_digit x p (b:bool) :=
+  if if leb 0 p then ltb p digits else false then
+    if b then lor x (lsl 1 p)
+    else land x (lxor max_int (lsl 1 p))
+  else x.
+
+(** Translation to Z *)
+Definition is_zero (i:int) := eqb i 0.
+Definition is_even (i:int) := is_zero (land i 1).
+Fixpoint to_Z_rec (n:nat) (i:int) :=
+  match n with
+  | O => 0
+  | S n =>
+      (if is_even i then Z.double else Z.succ_double) (to_Z_rec n (lsr i 1))
+  end.
+
+Definition to_Z := to_Z_rec size.
+
+Fixpoint of_pos_rec (n:nat) (p:positive) {struct p} :=
+  match n, p with
+  | O, _ => 0%uint63
+  | S n, xH => 1%uint63
+  | S n, xO p => lsl (of_pos_rec n p) 1
+  | S n, xI p => lor (lsl (of_pos_rec n p) 1) 1
+  end.
+
+Definition of_pos := of_pos_rec size.
+
+Definition of_Z z :=
+  match z with
+  | Zpos p => of_pos p
+  | 0 => 0%uint63
+  | Zneg p => sub 0 (of_pos p)
+  end.
+
+Definition wB := 2 ^ (Z.of_nat size).
+
+(* Bijection : uint63 <-> Bvector size *)
+
+Axiom of_to_Z : forall x, of_Z (to_Z x) = x.
+
+(** Specification of logical operations *)
+
+Axiom lsl_spec : forall x p, to_Z (lsl x p) = Z.modulo (to_Z x * 2 ^ to_Z p) wB.
+
+Axiom lsr_spec : forall x p, to_Z (lsr x p) = Z.div (to_Z x) (2 ^ to_Z p).
+
+Definition bit i n := negb (is_zero (lsl (lsr i n) (sub digits 1))).
+
+Axiom land_spec : forall x y i, bit (land x y) i = andb (bit x i) (bit y i).
+
+Axiom lor_spec : forall x y i, bit (lor x y) i = orb (bit x i) (bit y i).
+
+Axiom lxor_spec : forall x y i, bit (lxor x y) i = xorb (bit x i) (bit y i).
+
+(** Specification of basic opetations *)
+
+(* Arithmetic modulo operations *)
+
+(* Note: axioms would be simpler if we were using of_Z instead:
+   example: add_spec : forall x y, of_Z (x + y) = of_Z x + of_Z y. *)
+
+Axiom add_spec : forall x y, to_Z (add x y) = Z.modulo (to_Z x + to_Z y) wB.
+
+Axiom sub_spec : forall x y, to_Z (sub x y) = Z.modulo (to_Z x - to_Z y) wB.
+
+Axiom mul_spec : forall x y, to_Z (mul x y) = Z.modulo (to_Z x * to_Z y) wB.
+
+Axiom mulc_spec : forall x y,
+  to_Z x * to_Z y = to_Z (fst (mulc x y)) * wB + to_Z (snd (mulc x y)).
+
+Axiom div_spec : forall x y, to_Z (div x y) = Z.div (to_Z x) (to_Z y).
+
+Axiom mod_spec : forall x y,
+  to_Z (PrimInt63.mod x y) = Z.modulo (to_Z x) (to_Z y).
+
+(* Comparisons *)
+Axiom eqb_correct : forall i j, eqb i j = true -> i = j.
+
+Axiom eqb_refl : forall x, eqb x x = true.
+
+Axiom ltb_spec : forall x y, ltb x y = true <-> to_Z x < to_Z y.
+
+Axiom leb_spec : forall x y, leb x y = true <-> to_Z x <= to_Z y.
+
+(** Exotic operations *)
+
+(** Axioms on operations which are just short cut *)
+
+Definition compare_def x y :=
+  if ltb x y then Lt else if eqb x y then Eq else Gt.
+
+Axiom compare_def_spec : forall x y, compare x y = compare_def x y.
+
+Axiom head0_spec : forall x, 0 < to_Z x ->
+  Z.div wB 2 <= 2 ^ (to_Z (head0 x)) * to_Z x < wB.
+
+Axiom tail0_spec : forall x, 0 < to_Z x ->
+  exists y, Z0 <= y /\ to_Z x = (2 * y + 1) * (2 ^ to_Z (tail0 x)).
+
+Definition addc_def x y :=
+  let r := add x y in
+  if ltb r x then C1 r else C0 r.
+
+Axiom addc_def_spec : forall x y, addc x y = addc_def x y.
+
+Definition addcarry i j := add (add i j) 1.
+Definition addcarryc_def x y :=
+  let r := addcarry x y in
+  if leb r x then C1 r else C0 r.
+
+Axiom addcarryc_def_spec : forall x y, addcarryc x y = addcarryc_def x y.
+
+Definition subc_def x y := if leb y x then C0 (sub x y) else C1 (sub x y).
+
+Axiom subc_def_spec : forall x y, subc x y = subc_def x y.
+
+Definition subcarryc_def x y :=
+  if ltb y x then C0 (sub (sub x y) 1) else C1 (sub (sub x y) 1).
+
+Axiom subcarryc_def_spec : forall x y, subcarryc x y = subcarryc_def x y.
+
+Definition diveucl_def x y := (div x y, PrimInt63.mod x y).
+
+Axiom diveucl_def_spec : forall x y, diveucl x y = diveucl_def x y.
+
+Axiom diveucl_21_spec : forall a1 a2 b,
+  let (q,r) := diveucl_21 a1 a2 b in
+  let (q',r') := Z.div_eucl (to_Z a1 * wB + to_Z a2) (to_Z b) in
+  to_Z a1 < to_Z b -> to_Z q = q' /\ to_Z r = r'.
+
+Definition addmuldiv_def p x y :=
+  lor (lsl x p) (lsr y (sub digits p)).
+
+Axiom addmuldiv_def_spec : forall p x y,
+  addmuldiv p x y = addmuldiv_def p x y.

proux01/roots

theories/Array/PArray.v
@@ -1,68 +1,8 @@
-Require Import Uint63.
-
-Set Universe Polymorphism.
-
-Primitive array := #array_type.
-
-Primitive make : forall A, int -> A -> array A := #array_make.
-Arguments make {_} _ _.
-
-Primitive get : forall A, array A -> int -> A := #array_get.
-Arguments get {_} _ _.
-
-Primitive default : forall A, array A -> A:= #array_default.
-Arguments default {_} _.
-
-Primitive set : forall A, array A -> int -> A -> array A := #array_set.
-Arguments set {_} _ _ _.
-
-Primitive length : forall A, array A -> int := #array_length.
-Arguments length {_} _.
-
-Primitive copy : forall A, array A -> array A := #array_copy.
-Arguments copy {_} _.
-
-Module Export PArrayNotations.
-
-Declare Scope array_scope.
-Delimit Scope array_scope with array.
-Notation "t .[ i ]" := (get t i)
-  (at level 2, left associativity, format "t .[ i ]").
-Notation "t .[ i <- a ]" := (set t i a)
-  (at level 2, left associativity, format "t .[ i <- a ]").
-
-End PArrayNotations.
+From Stdlib Require Import Uint63.
+From Stdlib Require Export PrimArray ArrayAxioms.
 
 Local Open Scope uint63_scope.
 Local Open Scope array_scope.
-
-Primitive max_length := #array_max_length.
-
-(** Axioms *)
-Axiom get_out_of_bounds : forall A (t:array A) i, (i <? length t) = false -> t.[i] = default t.
-
-Axiom get_set_same : forall A t i (a:A), (i <? length t) = true -> t.[i<-a].[i] = a.
-Axiom get_set_other : forall A t i j (a:A), i <> j -> t.[i<-a].[j] = t.[j].
-Axiom default_set : forall A t i (a:A), default t.[i<-a] = default t.
-
-
-Axiom get_make : forall A (a:A) size i, (make size a).[i] = a.
-
-Axiom leb_length : forall A (t:array A), length t <=? max_length = true.
-
-Axiom length_make : forall A size (a:A),
-  length (make size a) = if size <=? max_length then size else max_length.
-Axiom length_set : forall A t i (a:A),
-  length t.[i<-a] = length t.
-
-Axiom get_copy : forall A (t:array A) i, (copy t).[i] = t.[i].
-Axiom length_copy : forall A (t:array A), length (copy t) = length t.
-
-Axiom array_ext : forall A (t1 t2:array A),
-  length t1 = length t2 ->
-  (forall i, i <? length t1 = true -> t1.[i] = t2.[i]) ->
-  default t1 = default t2 ->
-  t1 = t2.
 
 (* Lemmas *)
 

theories/Array/PrimArray.v
@@ -1,0 +1,36 @@
+From Stdlib Require Import PrimInt63.
+
+Set Universe Polymorphism.
+
+Primitive array := #array_type.
+
+Primitive make : forall A, int -> A -> array A := #array_make.
+Arguments make {_} _ _.
+
+Primitive get : forall A, array A -> int -> A := #array_get.
+Arguments get {_} _ _.
+
+Primitive default : forall A, array A -> A:= #array_default.
+Arguments default {_} _.
+
+Primitive set : forall A, array A -> int -> A -> array A := #array_set.
+Arguments set {_} _ _ _.
+
+Primitive length : forall A, array A -> int := #array_length.
+Arguments length {_} _.
+
+Primitive copy : forall A, array A -> array A := #array_copy.
+Arguments copy {_} _.
+
+Module Export PArrayNotations.
+
+Declare Scope array_scope.
+Delimit Scope array_scope with array.
+Notation "t .[ i ]" := (get t i)
+  (at level 2, left associativity, format "t .[ i ]").
+Notation "t .[ i <- a ]" := (set t i a)
+  (at level 2, left associativity, format "t .[ i <- a ]").
+
+End PArrayNotations.
+
+Primitive max_length := #array_max_length.

theories/BinNums/NatDef.v
@@ -1,0 +1,127 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+From Stdlib Require Export BinNums PosDef.
+
+(**********************************************************************)
+(** * Binary natural numbers, definitions of operations *)
+(**********************************************************************)
+
+Module N.
+
+(** ** Operation [x -> 2*x+1] *)
+
+Definition succ_double x :=
+  match x with
+  | N0 => Npos 1
+  | Npos p => Npos p~1
+  end.
+
+(** ** Operation [x -> 2*x] *)
+
+Definition double n :=
+  match n with
+  | N0 => N0
+  | Npos p => Npos p~0
+  end.
+
+(** ** The successor of a [N] can be seen as a [positive] *)
+
+Definition succ_pos (n : N) : positive :=
+ match n with
+   | N0 => xH
+   | Npos p => Pos.succ p
+ end.
+
+(** Subtraction *)
+
+Definition sub n m :=
+match n, m with
+| N0, _ => N0
+| n, N0 => n
+| Npos n', Npos m' =>
+  match Pos.sub_mask n' m' with
+  | Pos.IsPos p => Npos p
+  | _ => N0
+  end
+end.
+
+(** Order *)
+
+Definition compare n m :=
+  match n, m with
+  | N0, N0 => Eq
+  | N0, Npos m' => Lt
+  | Npos n', N0 => Gt
+  | Npos n', Npos m' => Pos.compare n' m'
+  end.
+
+(** Boolean equality and comparison *)
+
+Definition leb x y :=
+ match compare x y with Gt => false | _ => true end.
+
+(** Euclidean division *)
+
+Fixpoint pos_div_eucl (a:positive)(b:N) : N * N :=
+  match a with
+    | xH =>
+       match b with Npos 1 => (Npos 1, N0) | _ => (N0, Npos 1) end
+    | xO a' =>
+       let (q, r) := pos_div_eucl a' b in
+       let r' := double r in
+       if leb b r' then (succ_double q, sub r' b)
+        else (double q, r')
+    | xI a' =>
+       let (q, r) := pos_div_eucl a' b in
+       let r' := succ_double r in
+       if leb b r' then (succ_double q, sub r' b)
+        else  (double q, r')
+  end.
+
+(** Operation over bits of a [N] number. *)
+
+(** Logical [or] *)
+
+Definition lor n m :=
+ match n, m with
+   | N0, _ => m
+   | _, N0 => n
+   | Npos p, Npos q => Npos (Pos.lor p q)
+ end.
+
+(** Logical [and] *)
+
+Definition land n m :=
+ match n, m with
+  | N0, _ => N0
+  | _, N0 => N0
+  | Npos p, Npos q => Pos.land p q
+ end.
+
+(** Logical [diff] *)
+
+Definition ldiff n m :=
+ match n, m with
+  | N0, _ => N0
+  | _, N0 => n
+  | Npos p, Npos q => Pos.ldiff p q
+ end.
+
+(** [xor] *)
+
+Definition lxor n m :=
+  match n, m with
+    | N0, _ => m
+    | _, N0 => n
+    | Npos p, Npos q => Pos.lxor p q
+  end.
+
+End N.

theories/Numbers/Cyclic/Int63/Sint63Axioms.v
@@ -1,0 +1,44 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+(*            Benjamin Gregoire, Laurent Thery, INRIA, 2007             *)
+(************************************************************************)
+
+From Stdlib Require Import BinNums PosDef IntDef.
+From Stdlib Require Export PrimInt63 Uint63Axioms.
+
+Local Open Scope Z_scope.
+
+Local Notation "2" := (Zpos 2) : Z_scope.
+Local Infix "^" := Z.pow : Z_scope.
+Local Notation "x <= y" := (Z.compare x y <> Gt) : Z_scope.
+Local Notation "x < y" := (Z.compare x y = Lt) : Z_scope.
+
+Definition min_int := Eval vm_compute in (lsl 1 62).
+
+(** Translation to and from Z *)
+Definition to_Z (i : int) :=
+  if ltb i min_int then to_Z i
+  else Z.opp (to_Z (sub 0 i)).
+
+(** Specification of operations that differ on signed and unsigned ints *)
+
+Axiom asr_spec : forall x p, to_Z (asr x p) = Z.div (to_Z x) (2 ^ (to_Z p)).
+
+Axiom div_spec : forall x y,
+    to_Z x <> to_Z min_int \/ to_Z y <> Zneg 1 ->
+  to_Z (divs x y) = Z.quot (to_Z x) (to_Z y).
+
+Axiom mod_spec : forall x y, to_Z (mods x y) = Z.rem (to_Z x) (to_Z y).
+
+Axiom ltb_spec : forall x y, ltsb x y = true <-> to_Z x < to_Z y.
+
+Axiom leb_spec : forall x y, lesb x y = true <-> to_Z x <= to_Z y.
+
+Axiom compare_spec : forall x y, eqb x y = Z.eqb (to_Z x) (to_Z y).

theories/Numbers/Cyclic/Int63/Uint63Axioms.v
@@ -1,0 +1,175 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+(*            Benjamin Gregoire, Laurent Thery, INRIA, 2007             *)
+(************************************************************************)
+
+From Stdlib Require Import BinNums PosDef IntDef.
+From Stdlib Require Export PrimInt63.
+
+Local Open Scope Z_scope.
+
+Local Notation "0" := Z0 : Z_scope.
+Local Notation "1" := (Zpos 1) : Z_scope.
+Local Notation "2" := (Zpos 2) : Z_scope.
+Local Infix "+" := Z.add : Z_scope.
+Local Infix "-" := Z.sub : Z_scope.
+Local Infix "*" := Z.mul : Z_scope.
+Local Infix "^" := Z.pow : Z_scope.
+Local Notation "x <= y" := (Z.compare x y <> Gt) : Z_scope.
+Local Notation "x < y" := (Z.compare x y = Lt) : Z_scope.
+Local Notation "x <= y < z" := (x <= y /\ y < z) : Z_scope.
+
+Definition size := 63%nat.
+
+(** The number of digits as an int *)
+Definition digits := 63%uint63.
+
+(** The biggest int *)
+Definition max_int := Eval vm_compute in sub 0 1.
+
+(** Access to the nth digits *)
+Definition get_digit x p := ltb 0 (land x (lsl 1 p)).
+
+Definition set_digit x p (b:bool) :=
+  if if leb 0 p then ltb p digits else false then
+    if b then lor x (lsl 1 p)
+    else land x (lxor max_int (lsl 1 p))
+  else x.
+
+(** Translation to Z *)
+Definition is_zero (i:int) := eqb i 0.
+Definition is_even (i:int) := is_zero (land i 1).
+Fixpoint to_Z_rec (n:nat) (i:int) :=
+  match n with
+  | O => 0
+  | S n =>
+      (if is_even i then Z.double else Z.succ_double) (to_Z_rec n (lsr i 1))
+  end.
+
+Definition to_Z := to_Z_rec size.
+
+Fixpoint of_pos_rec (n:nat) (p:positive) {struct p} :=
+  match n, p with
+  | O, _ => 0%uint63
+  | S n, xH => 1%uint63
+  | S n, xO p => lsl (of_pos_rec n p) 1
+  | S n, xI p => lor (lsl (of_pos_rec n p) 1) 1
+  end.
+
+Definition of_pos := of_pos_rec size.
+
+Definition of_Z z :=
+  match z with
+  | Zpos p => of_pos p
+  | 0 => 0%uint63
+  | Zneg p => sub 0 (of_pos p)
+  end.
+
+Definition wB := 2 ^ (Z.of_nat size).
+
+(* Bijection : uint63 <-> Bvector size *)
+
+Axiom of_to_Z : forall x, of_Z (to_Z x) = x.
+
+(** Specification of logical operations *)
+
+Axiom lsl_spec : forall x p, to_Z (lsl x p) = Z.modulo (to_Z x * 2 ^ to_Z p) wB.
+
+Axiom lsr_spec : forall x p, to_Z (lsr x p) = Z.div (to_Z x) (2 ^ to_Z p).
+
+Definition bit i n := negb (is_zero (lsl (lsr i n) (sub digits 1))).
+
+Axiom land_spec : forall x y i, bit (land x y) i = andb (bit x i) (bit y i).
+
+Axiom lor_spec : forall x y i, bit (lor x y) i = orb (bit x i) (bit y i).
+
+Axiom lxor_spec : forall x y i, bit (lxor x y) i = xorb (bit x i) (bit y i).
+
+(** Specification of basic opetations *)
+
+(* Arithmetic modulo operations *)
+
+(* Note: axioms would be simpler if we were using of_Z instead:
+   example: add_spec : forall x y, of_Z (x + y) = of_Z x + of_Z y. *)
+
+Axiom add_spec : forall x y, to_Z (add x y) = Z.modulo (to_Z x + to_Z y) wB.
+
+Axiom sub_spec : forall x y, to_Z (sub x y) = Z.modulo (to_Z x - to_Z y) wB.
+
+Axiom mul_spec : forall x y, to_Z (mul x y) = Z.modulo (to_Z x * to_Z y) wB.
+
+Axiom mulc_spec : forall x y,
+  to_Z x * to_Z y = to_Z (fst (mulc x y)) * wB + to_Z (snd (mulc x y)).
+
+Axiom div_spec : forall x y, to_Z (div x y) = Z.div (to_Z x) (to_Z y).
+
+Axiom mod_spec : forall x y,
+  to_Z (PrimInt63.mod x y) = Z.modulo (to_Z x) (to_Z y).
+
+(* Comparisons *)
+Axiom eqb_correct : forall i j, eqb i j = true -> i = j.
+
+Axiom eqb_refl : forall x, eqb x x = true.
+
+Axiom ltb_spec : forall x y, ltb x y = true <-> to_Z x < to_Z y.
+
+Axiom leb_spec : forall x y, leb x y = true <-> to_Z x <= to_Z y.
+
+(** Exotic operations *)
+
+(** Axioms on operations which are just short cut *)
+
+Definition compare_def x y :=
+  if ltb x y then Lt else if eqb x y then Eq else Gt.
+
+Axiom compare_def_spec : forall x y, compare x y = compare_def x y.
+
+Axiom head0_spec : forall x, 0 < to_Z x ->
+  Z.div wB 2 <= 2 ^ (to_Z (head0 x)) * to_Z x < wB.
+
+Axiom tail0_spec : forall x, 0 < to_Z x ->
+  exists y, Z0 <= y /\ to_Z x = (2 * y + 1) * (2 ^ to_Z (tail0 x)).
+
+Definition addc_def x y :=
+  let r := add x y in
+  if ltb r x then C1 r else C0 r.
+
+Axiom addc_def_spec : forall x y, addc x y = addc_def x y.
+
+Definition addcarry i j := add (add i j) 1.
+Definition addcarryc_def x y :=
+  let r := addcarry x y in
+  if leb r x then C1 r else C0 r.
+
+Axiom addcarryc_def_spec : forall x y, addcarryc x y = addcarryc_def x y.
+
+Definition subc_def x y := if leb y x then C0 (sub x y) else C1 (sub x y).
+
+Axiom subc_def_spec : forall x y, subc x y = subc_def x y.
+
+Definition subcarryc_def x y :=
+  if ltb y x then C0 (sub (sub x y) 1) else C1 (sub (sub x y) 1).
+
+Axiom subcarryc_def_spec : forall x y, subcarryc x y = subcarryc_def x y.
+
+Definition diveucl_def x y := (div x y, PrimInt63.mod x y).
+
+Axiom diveucl_def_spec : forall x y, diveucl x y = diveucl_def x y.
+
+Axiom diveucl_21_spec : forall a1 a2 b,
+  let (q,r) := diveucl_21 a1 a2 b in
+  let (q',r') := Z.div_eucl (to_Z a1 * wB + to_Z a2) (to_Z b) in
+  to_Z a1 < to_Z b -> to_Z q = q' /\ to_Z r = r'.
+
+Definition addmuldiv_def p x y :=
+  lor (lsl x p) (lsr y (sub digits p)).
+
+Axiom addmuldiv_def_spec : forall p x y,
+  addmuldiv p x y = addmuldiv_def p x y.